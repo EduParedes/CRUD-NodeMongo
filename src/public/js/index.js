914a80bafdec4430f0c064ab7afe98607009ae3d
MelpCode <melp.code.01@gmail.com>
2022-10-11 19:17:08 -0500

    event delete menu button added

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,9 +1,21 @@
-console.log('Working');
 const btnDelete = document.getElementById('delete-menu');
-btnDelete.addEventListener("click",deleteMenu);
 
-function deleteMenu(){
-  if (confirm("Are you sure do you want to delete this menu?")){
-    return
+function deleteMenu(e){
+  e.preventDefault();
+  const id = btnDelete.dataset.id;
+  const currentUrl = window.location.href;
+  if(confirm('Are you sure do you want to delete this menu')){
+    fetch(`${currentUrl}/delete/${id}`,
+    {
+      method:'DELETE'
+    })
+    .then(res=>window.location.replace(currentUrl))
+    .catch(err=>console.log(err))
   }
 }
+
+if (btnDelete){
+  btnDelete.addEventListener("click",deleteMenu);
+}
+
+
